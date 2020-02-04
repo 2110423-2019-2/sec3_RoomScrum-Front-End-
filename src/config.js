c4673b72e6fb125808f2b43d0e2471c763d368ff
Rodchananat <rod8711@gmail.com>
2020-02-04 15:39:37 +0700

    fix: fix: misconfig  in `jsconfig.json`. fix synatx error in config.js

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
 const config = {
-  API_URL: process.env.REACT_APP_API_URL || 'http://localhost:3002';
+  API_URL: process.env.REACT_APP_API_URL || 'http://localhost:3002',
 };
 
 export default config;
