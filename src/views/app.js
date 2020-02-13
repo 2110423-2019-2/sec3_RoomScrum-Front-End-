cf9de0fe672c83eb6848eea3e02b3efe4aa5621a
Nonthakorn Taboonpong <catsfinetoo@gmail.com>
2020-02-13 22:33:18 +0700

    feat(Event): Make Raw Event Dashboard for Event Info, Applied Musician and Curren Contract

--- src/views/app.js
+++ src/views/app.js
@@ -3,6 +3,11 @@ import { BrowserRouter as Router, Route, Switch} from 'react-router-dom';
 import Login from './login';
 import NotFoundPage from './not-found';
 import Home from './home';
+import Register from './register';
+import Dashboard from './dashboard';
+import Eventinfo from './Event Info';
+import Appliedmusician from './AppliedMusician';
+import Currentcontract from './CurrentContract';
 
 export const App = () => {
   return (
@@ -14,9 +19,23 @@ export const App = () => {
         <Route exact path="/">
           <Home/>
         </Route>
+        <Route path="/dashboard">
+          <Dashboard/>
+        </Route>
+        <Route path="/eventinfo">
+          <Eventinfo/>
+        </Route>
+        <Route path="/appliedmusician">
+          <Appliedmusician/>
+        </Route>
+        <Route path="/currentcontract">
+          <Currentcontract/>
+        </Route>
         <Route path="/">
           <NotFoundPage/>
         </Route>
+       
+
       </Switch>
     </Router>
   );
