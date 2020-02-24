667261288ba6bd9c6c61592053945cf9028962d0
kanjeranian <oilscius@gmail.com>
2020-02-24 19:37:01 +0700

    dev: events advance search

--- src/views/event/find-events/find-events.js
+++ src/views/event/find-events/find-events.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useRef } from 'react';
 import { Navbar } from 'src/components/common';
 import { SearchEventsBox } from 'src/components/search-event-box';
 import { EventItem } from 'src/components/event-item';
@@ -8,7 +8,10 @@ import config from 'src/config';
 const FindEvents = () => {
   const [isFetch, setIsFetch] = useState(false);
   const [eventList, setEventList] = useState([]);
-  if (!isFetch) {
+  const keywordInput = useRef();
+  const searchInput = useRef();
+
+  const getEvents = () => {
     request
       .get(`${config.API_URL}/events`)
       .then(res => {
@@ -19,7 +22,26 @@ const FindEvents = () => {
       .catch(err => {
         alert(err);
       });
+  };
+
+  if (!isFetch) {
+    getEvents();
   }
+
+  const advanceSearch = () => {
+    request
+      .post(`${config.API_URL}/events/search`)
+      .send({
+        searchType: searchInput.current.value,
+        value: keywordInput.current.value
+      })
+      .then(res => {
+        setEventList(res.body);
+      })
+      .catch(err => {
+        getEvents();
+      });
+  };
 
   const eventItems = eventList.map(each => {
     return (
@@ -33,10 +55,25 @@ const FindEvents = () => {
     <div className='FindEvents'>
       <Navbar />
       <h1> FindEvents Page</h1>
-      <div className='row no-gutters'>
-        <div className='col-sm-3 border border-primary'>
-          <SearchEventsBox />
+      <div className='container'>
+        <div className='md-form mt-0 row'>
+          <input
+            className='form-control col-8'
+            ref={keywordInput}
+            type='text'
+          />
+          <select id='cars' className='col-2' ref={searchInput}>
+            <option value='default'>sarch by all</option>
+            <option value='name'>sarch by name</option>
+            <option value='description'>sarch by description</option>
+            <option value='location'>search by location</option>
+          </select>
+          <button className='col-2' onClick={advanceSearch}>
+            submitka
+          </button>
         </div>
+      </div>
+      <div className='row no-gutters'>
         <div className='col-sm border border-primary d-flex flex-wrap justify-content-between'>
           {eventItems}
         </div>
