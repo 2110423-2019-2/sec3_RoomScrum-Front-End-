9524953e870b5c17b7275e27b35c832fac3379fd
World <catsfinetoo@gmail.com>
2020-02-24 20:55:13 +0700

    feat(event):Create Applicants button which use to show AppliedMusicianList

--- src/views/app.js
+++ src/views/app.js
@@ -4,11 +4,16 @@ import Login from './login';
 import NotFoundPage from './not-found';
 import Home from './home';
 import FindEvents from './find-events';
+import RawAppliedMusician from './raw_applied-musician';
+import Applicants from './raw_applied-musician';
 
 export const App = () => {
   return (
     <Router>
       <Switch>
+      <Route path='/appliedmusician'>
+          <Applicants />
+        </Route>
         <Route path='/login'>
           <Login />
         </Route>
@@ -21,6 +26,7 @@ export const App = () => {
         <Route path='/'>
           <NotFoundPage />
         </Route>
+
       </Switch>
     </Router>
   );
