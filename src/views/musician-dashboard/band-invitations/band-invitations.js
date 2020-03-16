43a4ed7719354827d5cb13f89a3015ecc7c9d12f
Rodchananat K <rod8711@gmail.com>
2020-03-16 17:17:02 +0700

    feat(musician): finish band invitations without API

--- src/views/musician-dashboard/band-invitations/band-invitations.js
+++ src/views/musician-dashboard/band-invitations/band-invitations.js
@@ -1,18 +1,17 @@
 import React from 'react';
 import './band-invitations.scss';
 
-
-const BandInvitationItem = () => {
+const BandInvitationItem = ({}) => {
     return (
-        <div className="band-invite-item">
-            <img className="band-image" src="/logo192.png"/>
+        <div className="band-invite-item clearfix">
+            <img className="band-image" src="https://i.pravatar.cc/160"/>
             <div className="band-info">
                 <div className="name"> Band Name </div>
-                <div className="band-bio"> Band bio Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quasi ducimus quidem consequatur voluptatum voluptas tenetur quod optio. Repellat repellendus, voluptatibus officia perferendis adipisci itaque in, sit suscipit nemo similique rem.</div>
+                <div className="bio"> Band bio Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quasi ducimus quidem consequatur voluptatum voluptas tenetur quod optio. Repellat repellendus, voluptatibus officia perferendis adipisci itaque in, sit suscipit nemo similique rem.</div>
             </div>
             <div className="action-btns">
-                <button> Accept </button>
-                <button> Reject </button>
+                <button className="accept"> Accept </button>
+                <button className="reject"> Reject </button>
             </div>
         </div>
     )
@@ -20,7 +19,12 @@ const BandInvitationItem = () => {
 
 const BandInvitations = () => {
     return (
-        <div>
+        <div className="band-invitations">
+            <BandInvitationItem></BandInvitationItem>
+            <BandInvitationItem></BandInvitationItem>
+            <BandInvitationItem></BandInvitationItem>
+            <BandInvitationItem></BandInvitationItem>
+            <BandInvitationItem></BandInvitationItem>
             <BandInvitationItem></BandInvitationItem>
         </div>
     )
