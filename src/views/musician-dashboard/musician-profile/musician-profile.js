e085612d894f201be95745750035a7521cc7812b
Rodchananat K <rod8711@gmail.com>
2020-03-23 14:44:36 +0700

    feat(musician): code MusicianProfile comonent

--- src/views/musician-dashboard/musician-profile/musician-profile.js
+++ src/views/musician-dashboard/musician-profile/musician-profile.js
@@ -3,26 +3,92 @@ import request from 'superagent';
 import config from 'src/config';
 import { observer } from 'mobx-react';
 import { globalLoginState } from 'src/store';
+import Image from 'react-image';
+import moment from 'moment';
+import './musician-profile.scss';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faEdit } from '@fortawesome/free-solid-svg-icons';
 
-const MusicianProfile = ({musician}) => {
+moment.locale('en', {
+    relativeTime: {
+        future: "in %s",
+        past: "%s",
+        s: "now",
+        m: "1m",
+        mm: "%dm",
+        h: "1h",
+        hh: "%dh",
+        d: "1d",
+        dd: "%dd",
+        M: "1m",
+        MM: "%dm",
+        y: "1y",
+        yy: "%d"
+    }
+});
+
+const MusicianProfile = ({
+    musician: {
+        firstName, lastName,
+        username,
+        userId,
+        birthdate,
+        address, subdistrict, district, cityState, zipcode, country,
+        bio
+    }
+}) => {
     return (
-        <div>
-            musician profile works:
-            {JSON.stringify(musician)}
+        <div className="musician-profile">
+            <div className="title"> {firstName + ' ' + lastName} </div>
+            <div className="alias"> @{username} </div>
+            <Image className="profile-image" src={[
+                config.API_URL + `/user/profile-pic/${userId}`,
+                "https://i.pravatar.cc/180",
+            ]} />
+            <div className="desc">
+                <div className="label"> Birthdate </div>
+                <div className="value">
+                    {moment(birthdate).format('MMM DD, YYYY')} {" "}
+                    ( {moment(birthdate).fromNow()} years old )  </div>
+            </div>
+            <div className="desc">
+                <div className="label"> Location </div>
+                <div className="value">
+                    {address}, {subdistrict}, {district}, {cityState}, {zipcode}, {country}
+                </div>
+            </div>
+            <div className="desc">
+                <div className="label"> About </div>
+                <div className="value">
+                    {bio}
+                </div>
+            </div>
+            <button className="edit-profile-button">
+                <FontAwesomeIcon icon={faEdit} />
+                Edit my profile
+            </button>
         </div>
     )
 }
 
-const MusicianVideo = ({musician}) => {
+const MusicianVideo = ({ musician }) => {
     return (
-        <div>
-            musician video works
-            {JSON.stringify(musician)}
+        <div className="musician-video">
+            <div className="yt-wrapper">
+                <div className="yt-container">
+                    <iframe
+                        src="https://www.youtube.com/embed/KU4qOebhkfs"
+                        frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
+                        allowfullscreen
+                        className="yt-vid"
+                    ></iframe>
+                </div>
+            </div>
         </div>
     )
 }
 
-const _MusicianProfilePage = observer(({ loginState: {userId}}) => {
+const _MusicianProfilePage = observer(({ loginState: { userId } }) => {
     const [musicianInfo, setMusicianInfo] = useState(null);
     const lastFetched = useRef(null);
 
@@ -30,27 +96,25 @@ const _MusicianProfilePage = observer(({ loginState: {userId}}) => {
     if (userId && !lastFetched.current != userId) {
         lastFetched.current = userId;
         request.get(config.API_URL + '/user/' + userId)
-        .withCredentials()
-        .then(res => {
-            console.log(res.body);
-            setMusicianInfo(res.body);
-        })
-        .catch(err => {
-            alert(`error getting musician #${userId} info`);
-            console.error(`error getting musician #${userId} info`, err);
-        })
+            .withCredentials()
+            .then(res => {
+                console.log(res.body);
+                setMusicianInfo(res.body);
+            })
+            .catch(err => {
+                alert(`error getting musician #${userId} info`);
+                console.error(`error getting musician #${userId} info`, err);
+            })
     }
 
     return (
-        <div>
-            # {userId}
-            {JSON.stringify(musicianInfo)}
-            <MusicianProfile musician={musicianInfo}/>
-            <MusicianVideo musician={musicianInfo}/>
+        <div className="musician-profile-page">
+            {musicianInfo && <MusicianProfile musician={musicianInfo} />}
+            {musicianInfo && <MusicianVideo musician={musicianInfo} />}
         </div>
     )
 });
 
-const MusicianProfilePage = () => <_MusicianProfilePage loginState={globalLoginState}/>;
+const MusicianProfilePage = () => <_MusicianProfilePage loginState={globalLoginState} />;
 
 export default MusicianProfilePage;
