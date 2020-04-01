5402818db09a42c3cacf15e7fd989bdc535cee6f
World <catsfinetoo@gmail.com>
2020-04-01 15:21:12 +0700

    add MyReviews to be put in user profile

---
+++ src/views/review/my-reviews/my-reviews.js
@@ -0,0 +1,62 @@
+import React, { useRef, useState } from "react";
+//import "./register.scss";
+import { Navbar, Form } from "src/components/common";
+import request from "superagent";
+import config from "src/config";
+import {MyReviewsItem} from "src/components/my-reviews-item";
+
+const MyReviews = () => {
+
+    const [isFetch, setIsFetch] = useState(false);
+    const [myReviewsList, setMyReviewsList] = useState([]);
+
+    // const hirerId = () => {
+    //   request
+    //   .get(`${config.API_URL}/events`)
+    //   .then(res => {
+    //     setIsFetch(true);
+    //     setMyEventList(res.body);
+    //     console.log(res.body);
+    //   })
+    //   .catch(err => {
+    //     alert(err);
+    //   });
+
+    // }
+
+  
+    if (!isFetch) {
+      request
+      .post(`${config.API_URL}/events/find-my-event`)
+      .withCredentials()
+      .then(res => {
+        setIsFetch(true);
+        console.log(res.body);
+        setMyReviewsList(res.body);
+        // setMyEventList('res.body');
+        
+      })
+      .catch(err => {
+        alert(err);
+      });
+    }
+
+    const myReviewsItems = myReviewsList.map(each => {
+        return (
+          <div>
+            <MyReviewsItem each={each} />
+          </div>
+        );
+    });
+
+
+    return (
+        <div>
+            {myReviewsItems}
+        </div>
+    )
+    
+
+};
+
+export default MyReviews;
