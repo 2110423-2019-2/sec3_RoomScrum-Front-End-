765a0bce6ec70c385e4838e5633c965422e60bfb
Rodchananat K <rod8711@gmail.com>
2020-04-04 01:17:53 +0700

    feat(event-button): add complete event button

---
+++ src/components/action-buttons/complete-event-button.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import { ConfirmButton } from './base/confirm-button';
+import request from 'superagent';
+import config from 'src/config';
+
+// logic wrapper for complete event, you need to supply the children
+
+const CompleteEventButton = ({children, className, eventId, onSuccess, onFail}) => {
+    const completeEvent = () => {
+        if (!eventId) alert("Completed Event: No eventId supplied");
+        request.post(config.API_URL + `/event/${eventId}/complete`)
+            .withCredentials()
+            .then(res => {
+                alert("Completed Event OK");
+                onSuccess(res);
+            })
+            .catch(err => {
+                alert("Error: Completing Event");
+                console.group("Error Completing Event");
+                console.log("event id:", eventId);
+                console.error(err);
+                console.groupEnd();
+
+                onFail(err);
+            })
+    }
+    
+    return <ConfirmButton
+        children={children}
+        action={completeEvent}
+        className={className}
+        title={"Confirmation"}
+        question={"Mark Event Completed ?"}
+    />
+}
+
+export default CompleteEventButton;
