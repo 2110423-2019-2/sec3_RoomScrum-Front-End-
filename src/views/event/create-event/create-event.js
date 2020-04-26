da5b944aba03524d833f7dab88608b664f7dbc16
World <catsfinetoo@gmail.com>
2020-04-27 03:35:12 +0700

    Event: Try fixing create event but not work

--- src/views/event/create-event/create-event.js
+++ src/views/event/create-event/create-event.js
@@ -8,14 +8,101 @@ import request from "superagent";
 import config from "src/config";
 import Modal from "react-modal";
 import { ConfirmDialog } from "src/components/common";
-
 import { formBelow, formUpper } from "./form-definition";
+import FormV2 from "src/components/common/form/form-v2";
+import { formStateBuilder } from "src/components/common/form/form-state";
+import ImageUploader from "src/components/common/image-upload/image-upload";
+import { UserType } from "src/enums";
+
+// const CreateEvent = () => {
+//   const [eventFormBelow, dispatchEventFormBelow] = formStateBuilder(
+//     formBelow
+//   )();
+//   const [eventFormUpper, dispatchEventFormUpper] = formStateBuilder(
+//     formUpper
+//   )();
+//   const [eventImage, setEventImage] = useState(null);
+//   const [accepted, setAccepted] = useState(false);
+
+//   const submit = async () => {
+//     dispatchEventFormUooer({ type: "PRE_SUBMIT" });
+//     dispatchEventFormBelow({ type: "PRE_SUBMIT" });
+//     let hasError = false;
+//     for (let key in eventFormBelow) {
+//       console.log(eventFormBelow[key]);
+//       if (eventFormBelow[key].errors && eventFormBelow[key].errors.length > 0) {
+//         hasError = true;
+//         break;
+//       }
+//     }
+//     for (let key in eventFormUpper) {
+//       console.log(eventFormUpper[key]);
+//       if (eventFormUpper[key].errors && eventFormUpper[key].errors.length > 0) {
+//         hasError = true;
+//         break;
+//       }
+//     }
+//     if (hasError) {
+//       alert("Please enter all details");
+//       return;
+//     }
+//     const eventImageName = await request
+//       .post(`${config.API_URL}/user/temp-profile-pic`) // to change
+//       .attach("image", eventImage)
+//       .then((res) => {
+//         const { imageName } = JSON.parse(res.text);
+//         return imageName;
+//       })
+//       .catch((err) => {
+//         alert("profile upload err");
+//         console.log(err);
+//         return null;
+//       });
+//     if (!profileImageName) return;
+
+//     alert("upload files success");
+
+//     const sendData = {};
+//     for (let key in eventFormUpper)
+//       if (!formUpper[key].ignore) sendData[key] = eventFormUpper[key].value;
+//     for (let key in eventFormBelow)
+//       if (!formBelow[key].ignore) sendData[key] = eventFormBelow[key].value;
+
+//     for (let key in musicianForm)
+//       if (!musicianFormDef[key].ignore) sendData[key] = musicianForm[key].value;
+
+//     Object.assign(sendData, {
+//       eventImage: eventImageName,
+//     });
+
+//     try {
+//       await request
+//         .post(`${config.API_URL}/user/create`) //to change
+//         .send(sendData) 
+//         .then((res) => {
+//           const { token, username } = JSON.parse(res.text);
+//           if (token) {
+//             document.cookie = `token=${token}`;
+//             document.location = "/";
+//           } else {
+//             alert("invalid response from server");
+//           }
+//         })
+//         .catch((err) => {
+//           alert("error auto-logging in, please try login manually");
+//         });
+//     } catch (err) {
+//       alert("error");
+//     }
+//   };
+// };
 
 const CreateEventPage = () => {
   const uploadedFile = useRef();
   const formDataUpper = useRef();
   const formDataBelow = useRef();
   const [eventImage, setEventImage] = useState(null);
+  // const [eventId, setEventId] = useState(null);
 
   const clickUpload = () => {
     uploadedFile.current.click();
@@ -26,15 +113,21 @@ const CreateEventPage = () => {
     if (upload.files && upload.files[0]) {
       var reader = new FileReader();
 
-      reader.onload = e => {
+      reader.onload = (e) => {
         setEventImage(e.target.result);
       };
       reader.readAsDataURL(upload.files[0]);
     }
   };
 
-  const postData = async imageName => {
+  const postData = () => {
+    const eventId = 0;
     const data = {};
+    const form = new FormData();
+    const upload = uploadedFile.current;
+    if (!upload.files || !upload.files[0]) throw new Error("No file uploaded");
+    form.append("image", uploadedFile.current.files[0]);
+
     for (let key in formDataUpper.current) {
       if (
         key != "startDate" &&
@@ -47,7 +140,7 @@ const CreateEventPage = () => {
     for (let key in formDataBelow.current) {
       data[key] = formDataBelow.current[key].value;
     }
-    data["eventImage"] = imageName;
+    // data["eventImage"] = imageName;
     data["startdatetime"] =
       formDataUpper.current["startDate"].value +
       "T" +
@@ -60,32 +153,44 @@ const CreateEventPage = () => {
       ":00.000Z";
     console.log(JSON.stringify(data));
     request
-      .post(`${config.API_URL}/events`)
+      .post(`${config.API_URL}/events/create-event`)
       .withCredentials()
       .send(data)
-      .then(() => {
-        window.location.href = "/hirer/event";
+      .then((res) => {
+        // setEventId(res.body.eventId);
+        // const eventId = res.body.eventId;
+        const eventId = JSON.parse(res.eventId);
+        alert(res.body.eventId);
       })
-      .catch(err => console.log(err));
-  };
-
-  const uploadImage = async () => {
-    const form = new FormData();
-    const upload = uploadedFile.current;
-    if (!upload.files || !upload.files[0]) throw new Error("No file uploaded");
-    form.append("image", uploadedFile.current.files[0]);
-    return request
-      // .post(`${config.API_URL}/events/event-pic/${event.eventId}`)
+      .catch((err) => console.log(err));
+    console.log(`This is ${eventId}`);
+    request
+      .post(`${config.API_URL}/events/event-pic/${eventId}`)
       .withCredentials()
       .send(form)
-      .then(res => {
+      .then((res) => {
         return JSON.parse(res.text);
       });
   };
 
+  // const uploadImage = async () => {
+  //   const form = new FormData();
+  //   const upload = uploadedFile.current;
+  //   if (!upload.files || !upload.files[0]) throw new Error("No file uploaded");
+  //   form.append("image", uploadedFile.current.files[0]);
+  //   return request
+  //     .post(`${config.API_URL}/events/event-pic/${eventId}`)
+  //     .withCredentials()
+  //     .send(form)
+  //     .then(res => {
+  //       return JSON.parse(res.text);
+  //     });
+  // };
+
   const createEvent = async () => {
-    const res = await uploadImage();
-    await postData(res.imageName);
+    await postData();
+    // await uploadImage();
+    window.location.href = "/hirer/event";
   };
 
   const [showAlert, setAlert] = useState(false);
@@ -112,7 +217,7 @@ const CreateEventPage = () => {
                 <div
                   className={classnames({
                     overlay: true,
-                    "force-show": !eventImage
+                    "force-show": !eventImage,
                   })}
                   onClick={clickUpload}
                 >
@@ -131,7 +236,7 @@ const CreateEventPage = () => {
               <ConfirmDialog
                 title="Confirm?"
                 question="Do you want to create event"
-                callback={confirm => {
+                callback={(confirm) => {
                   setAlert(false);
                   if (confirm) {
                     // callbackAction.current();
@@ -152,8 +257,8 @@ const CreateEventPage = () => {
             </button>
           </div>
         </div>
-        </div>
       </div>
+    </div>
   );
 };
 
