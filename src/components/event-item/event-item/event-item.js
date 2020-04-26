172418eeb72a2c910e49c1485f1bbeb1cf3e9c8a
World <catsfinetoo@gmail.com>
2020-04-26 17:15:24 +0700

    Event: Fix bug in event search

--- src/components/event-item/event-item/event-item.js
+++ src/components/event-item/event-item/event-item.js
@@ -4,7 +4,7 @@ import "./event-item.scss";
 import request from "superagent";
 import config from "src/config";
 import Image from "react-image";
-import EventItemInfo from "./event-item-info"
+import EventItemInfo from "./event-item-info";
 
 const EventItem = ({
   each: {
@@ -45,8 +45,8 @@ const EventItem = ({
       marginTop: "auto",
       marginBottom: "auto",
       //   transform             : 'translate(-50%, -50%)',
-      width: "40%",
-      height: "50%",
+      width: "50%",
+      height: "70%",
     },
   };
   const handleApply = () => {
@@ -64,43 +64,48 @@ const EventItem = ({
   };
 
   return (
-    <div className="card event-item" onClick={openModal} style={{ width: 200 }}>
-      <div className="EventNameContainer">
-        <div className="EventName">{eventName}</div>
-      </div>
-      <Image
-        className="card-img-top"
-        style={{
-          width: 195,
-          height: 195,
-        }}
-        src={[
-          config.API_URL + `/events/${eventId}/pic`,
-          "https://i.pravatar.cc/180",
-        ]}
-        loader={<div className="event-image placeholder"></div>}
-      />
-      <div className=" Description">
-        <div className="Label">Time</div>
-        <div className="Value">
-          {startdatetime.substr(11, 8)} {startdatetime.substr(0, 10)} to{" "}
-          {enddatetime.substr(11, 8)} {enddatetime.substr(0, 10)}
+    <>
+      <div
+        className="card event-item"
+        onClick={openModal}
+        style={{ width: 200 }}
+      >
+        <div className="EventNameContainer">
+          <div className="EventName">{eventName}</div>
         </div>
-        <div className="Label">Subdistrict</div>
-        <div className="Value">{subdistrict}</div>
-        <div className="Label">District</div>
-        <div className="Value">{district}</div>
-
-        <Modal
-          isOpen={isOpen}
-          onAfterOpen={afterOpenModal}
-          onRequestClose={closeModal}
-          style={customStyles}
-        >
-          <EventItemInfo description={description} eventId={eventId}/>
-        </Modal>
+        <Image
+          className="card-img-top"
+          style={{
+            width: 195,
+            height: 195,
+          }}
+          src={[
+            config.API_URL + `/events/${eventId}/pic`,
+            "https://i.pravatar.cc/180",
+          ]}
+          loader={<div className="event-image placeholder"></div>}
+        />
+        <div className=" Description">
+          <div className="Label">Time</div>
+          <div className="Value">
+            {startdatetime.substr(11, 8)} {startdatetime.substr(0, 10)} to{" "}
+            {enddatetime.substr(11, 8)} {enddatetime.substr(0, 10)}
+          </div>
+          <div className="Label">Subdistrict</div>
+          <div className="Value">{subdistrict}</div>
+          <div className="Label">District</div>
+          <div className="Value">{district}</div>
+        </div>
       </div>
-    </div>
+      <Modal
+        isOpen={isOpen}
+        onAfterOpen={afterOpenModal}
+        onRequestClose={closeModal}
+        style={customStyles}
+      >
+        <EventItemInfo description={description} eventId={eventId} />
+      </Modal>
+    </>
   );
 };
 
