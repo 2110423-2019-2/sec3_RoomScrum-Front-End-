2db05fc6288422df193282e6d765702cc396af4d
World <catsfinetoo@gmail.com>
2020-04-28 02:19:10 +0700

    Remove Current Contract from Side bar

--- src/components/common/sidebar-hirer/sidebar-hirer.js
+++ src/components/common/sidebar-hirer/sidebar-hirer.js
@@ -7,7 +7,7 @@ import classname from 'classnames';
 const fakeItems = [
     {text: "My profile", href: "/hirer/profile/me"},
     {text: "My event", href: "/hirer/event"},
-    {text: "Current contract", href: "/hirer/contract"},
+    
 ] 
 
 const SideNavItem = ({item}) => {
