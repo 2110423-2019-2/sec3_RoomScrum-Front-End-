e257c8d3db42dc462d8d583e4776878269ab9c4c
World <catsfinetoo@gmail.com>
2020-04-28 19:55:48 +0700

    Hirer: Re-design event info to be like profile page

--- src/components/my-events-item/my-event-info/my-event-info.js
+++ src/components/my-events-item/my-event-info/my-event-info.js
@@ -1,22 +1,40 @@
-import React, { useState } from 'react';
-import Modal from 'react-modal';
-import request from 'superagent';
-import config from 'src/config';
-import Applicants from 'src/views/hirer-dashboard/raw-applied-musicians';
-import Edit from 'src/views/hirer-dashboard/raw-edit-event';
-import { Link } from 'react-router-dom';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-import { Navbar } from 'src/components/common';
-import { ConfirmDialog } from 'src/components/common';
-import './my-event-info.scss';
-import { ApplicationStatus, EventStatus } from 'src/enums';
-import Image from 'react-image';
-import MyMusician from './my-musician'
+import React, { useState } from "react";
+import Modal from "react-modal";
+import request from "superagent";
+import config from "src/config";
+import Applicants from "src/views/hirer-dashboard/raw-applied-musicians";
+import Edit from "src/views/hirer-dashboard/raw-edit-event";
+import { Link } from "react-router-dom";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { Navbar } from "src/components/common";
+import { ConfirmDialog } from "src/components/common";
+import "./my-event-info.scss";
+import { ApplicationStatus, EventStatus } from "src/enums";
+import Image from "react-image";
+import MyMusician from "./my-musician";
+import { ViewEventInfoButton } from "src/components/action-buttons/view-event-info-button";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faTimes } from "@fortawesome/free-solid-svg-icons";
+import {
+  HirerEventStatusIndicator,
+  ContractStatusIndicator,
+} from "src/components/event-item/status-indicator/status-indicator";
+import moment from "moment";
+
+const TimeDisplay = ({ start, end }) => {
+  return (
+    <div className="value">
+      {moment(start).format("h:mm DD/MM/YYYY")} -{" "}
+      {moment(end).format("h:mm DD/MM/YYYY")}
+    </div>
+  );
+};
+
 const MyEventInfo = ({ each, status, application }) => {
   if (application.contract) {
     console.log(application.contract.status);
   }
-  const [contractStatus, setContractStatus] = useState('');
+
   const {
     eventId,
     eventName,
@@ -31,6 +49,7 @@ const MyEventInfo = ({ each, status, application }) => {
     startdatetime,
     enddatetime,
     isCancelled,
+    budget,
     eventImage,
     userId,
   } = each;
@@ -38,7 +57,7 @@ const MyEventInfo = ({ each, status, application }) => {
   const [isOpen, setIsOpen] = useState(false);
   const openModal = () => {
     setIsOpen(true);
-    console.log('openModal');
+    console.log("openModal");
   };
   const afterOpenModal = () => {};
   const closeModal = () => {
@@ -47,64 +66,108 @@ const MyEventInfo = ({ each, status, application }) => {
 
   return (
     <div>
-      <div onClick={openModal} className='head-text'>
+      <div onClick={openModal} className="head-text">
         {each.eventName}
       </div>
 
       <Modal
         isOpen={isOpen}
         onAfterOpen={afterOpenModal}
-        onRequestClose={closeModal}>
-        <div className='Container'>
-          <div className='EventInfo col-6'>
-            <div className='Title'>{each.eventName}</div>
+        onRequestClose={closeModal}
+      >
+        <div className="Container">
+          {/* <ViewEventInfoButton application={application} /> */}
+          {/* <div className="EventInfo col-6">
+            <div className="Title">{each.eventName}</div>
             <Image
-              className='Image'
+              className="Image"
               src={[config.API_URL + `/events/${each.eventId}/pic`]}
             />
-            <div className='Description'>
-              <div className='Label'>Time</div>
-              <div className='Value'>
-                {each.startdatetime.substr(11, 8)}{' '}
-                {each.startdatetime.substr(0, 10)} to{' '}
-                {each.enddatetime.substr(11, 8)}{' '}
+            <div className="Description">
+              <div className="Label">Time</div>
+              <div className="Value">
+                {each.startdatetime.substr(11, 8)}{" "}
+                {each.startdatetime.substr(0, 10)} to{" "}
+                {each.enddatetime.substr(11, 8)}{" "}
                 {each.enddatetime.substr(0, 10)}
               </div>
             </div>
-            {/* <h6>Time</h6>
-            <p1>{each.startdatetime.substr(11,8)} {each.startdatetime.substr(0,10)} to {each.enddatetime.substr(11,8)} {each.enddatetime.substr(0,10)}</p1> */}
-            <div className='Description'>
-              <div className='Label'>Location</div>
-              <div className='Value'>
+
+            <div className="Description">
+              <div className="Label">Location</div>
+              <div className="Value">
                 {each.subdistrict} {each.district} {each.province}
               </div>
             </div>
-            <div className='Description'>
-              <div className='Label'>About</div>
-              <div className='Value'>{each.description}</div>
+            <div className="Description">
+              <div className="Label">About</div>
+              <div className="Value">{each.description}</div>
             </div>
             {(() => {
-              if (status == 'Created' || status == 'HaveApplicant') {
+              if (status == "Created" || status == "HaveApplicant") {
                 return (
-                  <div className='edit'>
+                  <div className="edit">
                     <Edit event={each} />
                   </div>
                 );
               }
             })()}
-            {/* <button onClick={() => doDelete(event.eventId)} > Cancel </button> */}
-          </div>
-          <div className='AppliedMusician col-5'>
-            {/* <Applicants eventId={each.eventId} /> */}
+          </div> */}
+
+          <div className="EventInfo">
+            <div className="Title"> {each.eventName}</div>
+            <div >
+              <Image className="EventInfoImage" src={[config.API_URL + `/events/${each.eventId}/pic`]} />
+            </div>
+
+            <div className="Description">
+              <div className="Label"> Address </div>
+              <div className="Value">
+                {" "}
+                {address +
+                  ", " +
+                  subdistrict +
+                  ", " +
+                  district +
+                  ", " +
+                  province +
+                  ", " +
+                  zipcode}
+              </div>
+            </div>
+            <div className="Description">
+              <div className="Label"> Time </div>
+              <TimeDisplay start={startdatetime} end={enddatetime} />
+            </div>
+            <div className="Description">
+              <div className="Label"> Budget </div>
+              <div className="Value"> {budget || "<<budget>>"} baht </div>
+            </div>
+            <div className="Description">
+              <div className="Label"> About </div>
+              <div className="Value"> {description}</div>
+            </div>
             {(() => {
-              if (status == 'Created') {
-                return <div className='NoApllicant'>You have no applicant</div>;
+              if (status == "Created" || status == "HaveApplicant") {
+                return (
+                  <div className="edit">
+                    <Edit event={each} />
+                  </div>
+                );
+              }
+            })()}
+          </div>
+
+
+          <div className="AppliedMusician darknavy-bg ">
+            {(() => {
+              if (status == "Created") {
+                return <div className="NoApllicant">You have no applicant</div>;
               }
               if (
-                status == 'HaveApplicant' ||
+                status == "HaveApplicant" ||
                 (application.contract &&
-                  application.contract.status == 'Cancelled')
-                // ||application.contract.status == 'Cancelled'
+                  application.contract.status == "Cancelled")
               ) {
                 return (
                   <div>
@@ -112,9 +175,10 @@ const MyEventInfo = ({ each, status, application }) => {
                   </div>
                 );
               }
-              if (status == 'ContractDrafting') {
-                // return <MyMusician eventId={eventId}/>; //can't use
-                return <div className='ShowMusician'>You have accepted musician</div>;
+              if (status == "ContractDrafting") {
+                return (
+                  <div className="ShowMusician">You have accepted musician</div>
+                );
               }
             })()}
           </div>
